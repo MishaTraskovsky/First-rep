ab48dca231adc654c07ef648f419abc731d10bae
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-08 17:21:54 +0300

    framework

---
+++ g2/deploy.pl
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+echo "Content-type: text/plain\n";
+echo '';
+cd /var/www/domains/m.qzo.su/ || exit > /dev/null
+git reset --hard > /dev/null
+git pull > /dev/null
+chmod -R 755 /var/www/domains/m.qzo.su > /dev/null
