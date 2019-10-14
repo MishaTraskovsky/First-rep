eabeb297f2ec940f03a09543c3d4e2f356988be9
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-14 16:23:20 +0300

    14.10

--- app/users/PublicUsersController.php
+++ app/users/PublicUsersController.php
@@ -10,7 +10,7 @@ class PublicUsersController{
 
     public function gettest(){ 
     $class = new Users();
-    $class -> add_user_cards(); 
-    $class -> test2();
+    $class -> add_users_person_data(); 
+    $class -> view_users_person_data();
     }
 }
--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -1,12 +1,20 @@
 <?php
  
 class Users extends Model{
-    public function test2() {
+    public function view_users_cards() {
         $bd = Model::table("users_cards")->get()->send();
+        $this->viewJSON($bd);
+        }
+    public function view_users_person_data() {
+        $bd = Model::table("users_person_data")->get()->send();
         $this->viewJSON($bd);
         }
     public function add_user_cards(){
         Model::table("users_cards")->add(array("level" => $_GET["level"], "user_type" => $_GET["user_type"], "image" => $_GET["image"], "nickname" => $_GET["nickname"], "rating" => $_GET["rating"], "description" => $_GET["description"]))->send();
+    }
+    
+    public function add_users_person_data(){
+        Model::table("users_person_data")->add(array("password" => $_GET["password"], "phone" => $_GET["phone"],"phone_token" => $_GET["phone_token"],"phone_token_data" => $_GET["phone_token_data"],"doc_photo" => $_GET["doc_photo"],"surname" => $_GET["surname"],"name" => $_GET["name"],"patronymic" => $_GET["patronymic"],"date_of_birth" => $_GET["date_of_birth"],"gender" => $_GET["gender"],"other_data" => $GET["other_data"]))->send();      
     }
 
 }
