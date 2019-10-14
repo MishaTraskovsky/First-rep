10a6ec38f07c4847922cb1ec32855b0aca9989d1
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-14 15:21:35 +0300

    14.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -2,7 +2,7 @@
  
 class Users extends Model{
     public function test2() {
-        public $ar = array( 'id1' => '1',
+        $ar = array( 'id1' => '1',
 			         'id2' => '2',
 			         'id3' => '3',);
         echo json_incode($ar);
