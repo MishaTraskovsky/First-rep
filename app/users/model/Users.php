d61412100d4a2541823952c681226452de6cd5ca
Misha Traskovsky <traskovskymisha@yandex.ru>
2019-10-14 10:53:23 +0300

    14.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -4,4 +4,12 @@ class Users extends Model{
     public function test2() {
      require_once 'include/bd.php';
     }   
-}
+
+    public function get_t(){
+        $sql = "SELECT * FROM `users_cards`";
+        $result = mysqli_query($l, $sql);
+        echo '<pre>';
+        var_dump($result)
+        echo '<pre>';
+    }
+} 
