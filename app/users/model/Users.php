601469143618b60a585570d6501ff2febecd5a4e
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-14 15:46:18 +0300

    14.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -2,13 +2,7 @@
  
 class Users extends Model{
     public function test2() {
-       $ar = array(
-
-        '03-13-2012' => 'text1',
-        '07-19-2012' => 'text2',
-
-        );
-        $this->viewJSON($ar);
-        //Model::table("users_person_data")->get()->send()
+        $bd = Model::table("users_person_data")->get()->send();
+        $this->viewJSON($bd);
         }
 }
