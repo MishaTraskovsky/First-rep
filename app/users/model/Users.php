da4ca143680e70357f84f430e7e47e1b061e87e8
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-14 15:58:23 +0300

    14.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -4,8 +4,8 @@ class Users extends Model{
     public function test2() {
         $bd = Model::table("users_person_data")->get()->send();
         $this->viewJSON($bd);
-        };
-    public function add_user_cards {
+        }
+    public function add_user_cards(){
         Model::table("users_cards")->add(array("level" => "$_GET[level]", "user_type" => "$_GET[user_type]", "image" => "$_GET[image]", "nickname" => "$_GET[nickname]", "rating" => "$_GET[rating]", "description" => "$_GET[description]"))->send();
     }
 
