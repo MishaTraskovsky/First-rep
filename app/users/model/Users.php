7c7702c3c65f2ca3d759cd27c81b77e9a5a0dddc
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-15 14:39:07 +0300

    15.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -98,5 +98,25 @@ class Users extends Model{
             $this->viewJSON($rows);
 
         }
+    
+        public function ins_users_person_data_sql() {
+            $stmt = self::$db->prepare("NSERT INTO `users_person_data`(`password`, `phone`, `phone_token`, `phone_token_data`, `doc_photo`, `surname`, `name`, `patronymic`, `date_of_birth`, `gender`, `other_data`) VALUES (":password",":phone",":phone_token",":phone_token_data",":doc_photo",":surname",":name",":patronymic",":date_of_birth",":gender",":other_data")");
+
+            $result_query = $stmt->execute(array(":password" => self::$params_url['password'],
+                                                 ":phone" => self::$params_url['phone'],
+                                                 ":phone_token" => self::$params_url['phone_token'],
+                                                 ":phone_token_data" => self::$params_url['phone_token_data'],
+                                                 ":doc_photo" => self::$params_url['doc_photo'],
+                                                 ":surname" => self::$params_url['surname'],
+                                                 ":name" => self::$params_url['name'],
+                                                 ":date_of_birth" => self::$params_url['date_of_birth'],
+                                                 ":gender" => self::$params_url['gender'],
+                                                 ":other_data" => self::$params_url['other_data'],));
+
+            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+            $this->viewJSON($rows);
+
+        }
 
 }
