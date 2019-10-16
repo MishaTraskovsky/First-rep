9c8d749da6ca98b53cb24ece6ed2de5e489e17c9
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-16 16:31:44 +0300

    15.10_h

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -26,7 +26,7 @@ class Users extends Model{
         }
         
         else{
-        $c = mysql_query("SELECT COUNT(1) FROM users_cards");
+        $c = self::$db->prepare("SELECT COUNT(1) FROM users_cards");
         echo "Пользователь " . $c ." успешно создан";
         Model::table("users_cards")->add(array("level" => $_GET["level"],
                                                "user_type" => $_GET["user_type"],
