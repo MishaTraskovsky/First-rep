e37ceea4d877467fff385e3ac6bee233d12e835b
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-22 17:09:47 +0300

    22.10

--- app/users/model/Users.php
+++ app/users/model/Users.php
@@ -196,7 +196,7 @@ class Users extends Model{
 
         $result_query = $stmt->execute(array(":id_card" => self::$params_url['id_card']));    
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo $rows;
+        $this->viewJSON($rows);
         //$GetID = Model::table("users_person_data")->edit(array("id_card" => $_GET["id_card"],
         //                                                       "password" => $_GET["password"],
         //                                                       "phone" => $_GET["phone"],
