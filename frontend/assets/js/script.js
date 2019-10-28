c13468cdcc266fe4ec154159bf25883de8345763
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-28 14:26:15 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,1 +1,15 @@
-alert("connect");
+jquery(function(){
+        
+    $('btn btn-primary').click(function addRow(id){                     
+        var tbody = document.getElementById(id).getElementsByTagName("TBODY")[0];
+        var row = document.createElement("TR")
+        var td1 = document.createElement("TD")
+        td1.appendChild(document.createTextNode("column 1"))
+        var td2 = document.createElement("TD")
+        td2.appendChild (document.createTextNode("column 2"))
+        row.appendChild(td1);
+        row.appendChild(td2);
+        tbody.appendChild(row);
+
+    });  
+});
