8c9e3b30fb9f8cdc06ec655ad9f2bc09ba051997
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-28 17:04:43 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
         
-   //$('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_card</th><th scope="col">password</th><th scope="col">phone</th><th scope="col">phone_token</th><th scope="col">phone_token_data</th><th scope="col">doc_photo</th><th scope="col">surname</th><th scope="col">name</th><th scope="col">patronymic</th><th scope="col">date_of_birth</th><th scope="col">gender</th><th scope="col">other_data</th></tr></thead>');
+   $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_card</th><th scope="col">password</th><th scope="col">phone</th><th scope="col">phone_token</th><th scope="col">phone_token_data</th><th scope="col">doc_photo</th><th scope="col">surname</th><th scope="col">name</th><th scope="col">patronymic</th><th scope="col">date_of_birth</th><th scope="col">gender</th><th scope="col">other_data</th></tr></thead>');
     
     $('#addraw').click(function() {
        $('.table').append('<tr><th scope="row">5</th> <td>newname</td> <td>newsurname</td> <td>@newmale</td></tr>');    
@@ -18,12 +18,12 @@ $(document).ready(function(){
  type: 'GET',
  datatype: 'json',
  data: {},
- success: function drawTable(data) {
+ success: function (data) {
   var html = '';
   for (var i = 0; i < data.length; i++) {
     html += '<tbody><tr><td>' + data[i].id + '</td><td>' + data[i].id_card + '</td><td>' + data[i].password + '</td><td>' + data[i].phone + '</td><td>' + data[i].phone_token + '</td><td>' + data[i].phone_token_data + '</td><td>' + data[i].doc_photo + '</td><td>' + data[i].surname + '</td><td>' + data[i].name + '</td><td>' + data[i].patronymic + '</td><td>' + data[i].date_of_birth + '</td><td>' + data[i].gender + '</td><td>' + data[i].other_data + '</td></tr></tbody>';
   }
-    console.log("1");
+    console.log(html);
   $('.upd').append(html);
 },
  error:  function() {
