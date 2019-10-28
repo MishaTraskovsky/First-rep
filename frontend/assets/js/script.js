72f4d34c7becec5614abc743ec876e135b8ae667
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-28 15:02:13 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     });
     
     $('#clear').click(function() {
-       $('.table').remove();    
+       $('.table').empty();    
         console.log("clear");
     });
 });
