87589e097543e6fa22e25dc1f0907555326e4a3f
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-28 14:58:39 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
         
     $('#addraw').click(function() {
-       $('table').append('<tr><th scope="row">5</th> <td>newname</td> <td>newsurname</td> <td>@newmale</td></tr>');    
+       $('.table-dark').append('<tr><th scope="row">5</th> <td>newname</td> <td>newsurname</td> <td>@newmale</td></tr>');    
         console.log("addrow");
     });
 });
