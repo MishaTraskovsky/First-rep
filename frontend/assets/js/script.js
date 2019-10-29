854232cdc87af64d536e175f1d1aeaf11653c29b
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-29 15:36:26 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -37,26 +37,26 @@ $.ajax({
  
 
 //$('#sortA').click(
-//    $.ajax({
-        url: 'https://m.qzo.su/api/users/viewUPDsortA', 
-        type: 'GET',
-        datatype: 'json',
-        data: {},
-        success:    function(){
-                    //console.log("Sort by ASC succsess"),
-        table( data);
-        }
-        }))
+  //  $.ajax({
+  //      url: 'https://m.qzo.su/api/users/viewUPDsortA', 
+  //      type: 'GET',
+  //      datatype: 'json',
+  //      data: {},
+  //      success:    function(){
+  //                  //console.log("Sort by ASC succsess"),
+  //      table( data);
+  //      }
+  //      }))
  
 //$('#sortD').click(
-//    $.ajax({
-        url: 'https://m.qzo.su/api/users/viewUPDsortD', 
-        type: 'GET',
-        datatype: 'json',
-        data: {},
-        success: function (){
-                    table( data);
-                    //console.log("Sort by DESC succsess");
-                 }   
+  //  $.ajax({
+  //      url: 'https://m.qzo.su/api/users/viewUPDsortD', 
+  //      type: 'GET',
+  //      datatype: 'json',
+  //      data: {},
+  //      success: function (){
+  //                  table( data);
+  //                  //console.log("Sort by DESC succsess");
+  //               }   
 }))
 });
