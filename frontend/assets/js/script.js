6df5dfdec7a2b82285c7774df591015b8c9aabfe
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-29 15:44:32 +0300

    28.10

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -41,7 +41,7 @@ $.ajax({
         type: 'GET',
         datatype: 'json',
         data: {},
-        success:    function(){
+        success:    function(data){
                     table ( data);
                     $('.table').empty();
                     $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_card</th><th scope="col">password</th><th scope="col">phone</th><th scope="col">phone_token</th><th scope="col">phone_token_data</th><th scope="col">doc_photo</th><th scope="col">surname</th><th scope="col">name</th><th scope="col">patronymic</th><th scope="col">date_of_birth</th><th scope="col">gender</th><th scope="col">other_data</th></tr></thead>');
@@ -56,7 +56,7 @@ $.ajax({
         type: 'GET',
         datatype: 'json',
         data: {},
-        success:    function(){
+        success:    function(data){
                     table ( data);
                     $('.table').empty();
                     $('.table').append('<thead><tr><th scope="col">id</th><th scope="col">id_card</th><th scope="col">password</th><th scope="col">phone</th><th scope="col">phone_token</th><th scope="col">phone_token_data</th><th scope="col">doc_photo</th><th scope="col">surname</th><th scope="col">name</th><th scope="col">patronymic</th><th scope="col">date_of_birth</th><th scope="col">gender</th><th scope="col">other_data</th></tr></thead>');
