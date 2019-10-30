bef9e3993f7653120a9deab5791342a3838c6a1c
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-30 17:35:11 +0300

    3010

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -68,31 +68,31 @@ $('.sortD').click(function(){
 });
     
     $('#add').click(function(){
-        var id = $('#id_card').val();
-        var password = $('#password').val();
-        var phone = $('#phone').val();
-        var phone_token = $('#phone_token').val();
-        var phone_token_data = $('#phone_token_data').val();
-        var doc_photo = $('#doc_photo').val();
-        var surname = $('#surname').val();
-        var name = $('#name').val();
-        var patronymic = $('#patronymic').val();
-        var date_of_birth = $('#date_of_birth').val();
-        var gender = $('#gender').val();
-        var other_data = $('#other_data').val();
+        var id_card = $('#id_card').val();
+//        var password = $('#password').val();
+//        var phone = $('#phone').val();
+//        var phone_token = $('#phone_token').val();
+//        var phone_token_data = $('#phone_token_data').val();
+//        var doc_photo = $('#doc_photo').val();
+//        var surname = $('#surname').val();
+//        var name = $('#name').val();
+//        var patronymic = $('#patronymic').val();
+//        var date_of_birth = $('#date_of_birth').val();
+//        var gender = $('#gender').val();
+//        var other_data = $('#other_data').val();
         
         $('#id_card').val('');
-        $('#password').val('');
-        $('#phone').val('');
-        $('#phone_token').val('');
-        $('#phone_token_data').val('');
-        $('#doc_photo').val('');
-        $('#surname').val('');
-        $('#name').val('');
-        $('#patronymic').val('');
-        $('#date_of_birth').val('');
-        $('#gender').val('');
-        $('#other_data').val('');
+//        $('#password').val('');
+//        $('#phone').val('');
+//        $('#phone_token').val('');
+//        $('#phone_token_data').val('');
+//        $('#doc_photo').val('');
+//        $('#surname').val('');
+//        $('#name').val('');
+//        $('#patronymic').val('');
+//        $('#date_of_birth').val('');
+//        $('#gender').val('');
+//        $('#other_data').val('');
         
         $.ajax({
             url: 'https://m.qzo.su/api/users/addUPDforPkey', 
@@ -100,7 +100,7 @@ $('.sortD').click(function(){
             datatype: 'json',
             cache: false,
             contentType: false,
-            data: {id_card: id_card, phone: phone},
+            data: {id_card: 2},
             success:   console.log("New entry added")
             });
     })
