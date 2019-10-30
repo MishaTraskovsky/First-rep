adafe2a19ae67124561626d420e8b474d106fa61
Misha Traskovskij <amdmmisha@gmail.com>
2019-10-30 17:24:28 +0300

    3010

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -99,11 +99,7 @@ $('.sortD').click(function(){
             type: 'GET',
             datatype: 'json',
             data: {id_card: id_card, phone: phone},
-            success:  function(data){
-                   console.log("New entry added"); 
-                      }
-                }) 
-                });
-    
-    });    
+            success:   console.log("New entry added"); 
+            });
+    })
 });
